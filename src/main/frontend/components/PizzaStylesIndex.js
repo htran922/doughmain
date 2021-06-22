ecdc25097246a82d21bc18e45d8f39e9fdbc45bc
Helen <helentran.dev@gmail.com>
2021-06-22 14:43:18 -0400

    Created a find all pizza styles method in PizzaStyleService

--- src/main/frontend/components/PizzaStylesIndex.js
+++ src/main/frontend/components/PizzaStylesIndex.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react"
 
+import PizzaStyleTile from "./PizzaStyleTile";
+
 const PizzaStylesIndex = props => {
   const [pizzaStyles, setPizzaStyles] = useState([])
 
--- src/main/java/com/launchacademy/reviews/controllers/PizzaStylesApiV1Controller.java
+++ src/main/java/com/launchacademy/reviews/controllers/PizzaStylesApiV1Controller.java
@@ -1,6 +1,10 @@
 package com.launchacademy.reviews.controllers;
 
+import com.launchacademy.reviews.models.PizzaStyle;
+import com.launchacademy.reviews.services.PizzaStyleService;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
