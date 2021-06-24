e6cb90bd49685e99857a575a8bdcc3ccb6de4b91
Darleen Brazoban <darleenbrazoban@gmail.com>
2021-06-24 11:29:15 -0400

    Added 404 component

---
+++ src/main/frontend/components/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NotFoundPage extends React.Component{
+    render(){
+        return <div className="error-page">
+            <img src={"https://miro.medium.com/max/1068/1*wUOrpv-selJOytCkslSIhg.png"} className="pet-image" />
+            <h2>Page Not Found!</h2>
+          </div>;
+    }
+}
+export default NotFoundPage;
--- src/main/java/com/launchacademy/reviews/controllers/HomeController.java
+++ src/main/java/com/launchacademy/reviews/controllers/HomeController.java
@@ -1,11 +1,17 @@
 package com.launchacademy.reviews.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-  @GetMapping(value = {"/pizza-styles", "/pizza-styles/{id}", "/pizza-styles/new"})
+  @GetMapping(value = {"/pizza-styles", "/pizza-styles/{id}", "/pizza-styles/new", "/404"})
   public String forward() {
     return "forward:/";
   }
+
+    @RequestMapping(value = "/**/{path:[^\\\\.]*}")
+    public String redirect() {
+        return "forward:/404";
+    }
 }
