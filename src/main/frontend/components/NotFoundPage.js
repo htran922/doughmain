ee4016397a5fb8ff9b9b59a0bba748307fe6f8ba
Darleen Brazoban <darleenbrazoban@gmail.com>
2021-06-24 12:56:25 -0400

    Fixed spacing

--- src/main/frontend/components/NotFoundPage.js
+++ src/main/frontend/components/NotFoundPage.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react"
 
-class NotFoundPage extends React.Component{
-    render(){
-        return <div className="error-page">
-            <img src={"https://miro.medium.com/max/1068/1*wUOrpv-selJOytCkslSIhg.png"} className="pizza-image" />
-            <h2>Page Not Found!</h2>
-          </div>;
-    }
+class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <div className="error-page">
+        <img
+          src={"https://miro.medium.com/max/1068/1*wUOrpv-selJOytCkslSIhg.png"}
+          className="pizza-image"
+        />
+        <h2>Page Not Found!</h2>
+      </div>
+    )
+  }
 }
-export default NotFoundPage;
+export default NotFoundPage
