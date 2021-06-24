f50b418dba93537f7902c7f3ea73328e99ff457c
Darleen Brazoban <darleenbrazoban@gmail.com>
2021-06-24 18:42:20 -0400

    Added more data to seeders and removed a sout

--- src/main/java/com/launchacademy/reviews/seeders/PizzaStyleSeeder.java
+++ src/main/java/com/launchacademy/reviews/seeders/PizzaStyleSeeder.java
@@ -21,14 +21,41 @@ public class PizzaStyleSeeder {
       pizzaStyle.setImgUrl(
           "https://cdnimg.webstaurantstore.com/uploads/buying_guide/2014/11/pizzatypes-deepdish.jpg");
       service.save(pizzaStyle);
+
       pizzaStyle = new PizzaStyle();
       pizzaStyle.setName("Neapolitan");
       pizzaStyle.setImgUrl(
           "https://cdnimg.webstaurantstore.com/uploads/buying_guide/2014/11/pizzatypes-margherita-.jpg");
       service.save(pizzaStyle);
+
       pizzaStyle = new PizzaStyle();
       pizzaStyle.setName("Sicilian");
       pizzaStyle.setImgUrl("https://cdnimg.webstaurantstore.com/uploads/blog/2016/8/rectangle.jpg");
+      service.save(pizzaStyle);
+
+      pizzaStyle = new PizzaStyle();
+      pizzaStyle.setName("New York-Style");
+      pizzaStyle.setImgUrl("https://cdnimg.webstaurantstore.com/uploads/blog/2016/8/flat.jpg");
+      service.save(pizzaStyle);
+
+      pizzaStyle = new PizzaStyle();
+      pizzaStyle.setName("Greek");
+      pizzaStyle.setImgUrl("https://cdnimg.webstaurantstore.com/uploads/blog/2016/8/onions.jpg");
+      service.save(pizzaStyle);
+
+      pizzaStyle = new PizzaStyle();
+      pizzaStyle.setName("California");
+      pizzaStyle.setImgUrl("https://cdnimg.webstaurantstore.com/uploads/buying_guide/2014/11/pizzatypes-gourmet.jpg");
+      service.save(pizzaStyle);
+
+      pizzaStyle = new PizzaStyle();
+      pizzaStyle.setName("Detroit");
+      pizzaStyle.setImgUrl("https://cdnimg.webstaurantstore.com/uploads/blog/2019/3/blog-types-pizza_in-blog-7.jpg");
+      service.save(pizzaStyle);
+
+      pizzaStyle = new PizzaStyle();
+      pizzaStyle.setName("St. Louis");
+      pizzaStyle.setImgUrl("https://cdnimg.webstaurantstore.com/uploads/blog/2019/3/blog-types-pizza_in-blog-8.jpg");
       service.save(pizzaStyle);
     }
   }
--- src/main/java/com/launchacademy/reviews/seeders/ReviewSeeder.java
+++ src/main/java/com/launchacademy/reviews/seeders/ReviewSeeder.java
@@ -16,16 +16,23 @@ public class ReviewSeeder {
 
   public void run(){
     if(service.count() == 0) {
-      service.save(new Review("First Review", "This being my first review, I'll keep it brief.",
+      service.save(new Review("Deepest Nightmare", "Chicago pizza is just too much. Like an inferior lasagna.",
           0, "https://francospizza.com/wp-content/uploads/banner__pizza-1.png"
           ), 1);
-      service.save(new Review("Second Review", "My best second review of a great pizza.",
-          3,
+      service.save(new Review("My Cheesy Dream!", "I can never have enough cheese on my pizza and deep dish alwaaaaays hits the spot.",
+          5, "https://images.unsplash.com/photo-1582476927499-65372fb1a458?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          ), 1);
+      service.save(new Review("Neat-O-Politan", "The freshest pizza ever! The Margherita version is a must-try!",
+          5,
           "https://media.istockphoto.com/photos/cheesy-pepperoni-pizza-picture-id938742222?k=6&m=938742222&s=612x612&w=0&h=on_9ZYG1SG4Xgk7BLZSlaXJl8VYb6ZePDHTN6zukDHM="), 2);
       service.save(
-          new Review("Third Review", "Fantastic Pizza that won't burn the roof of your mouth!",
-              5,
+          new Review("Meh", "It's an ok pizza, depending on where you order it from.",
+              3,
               "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/27/0/KC1812_Spaghetti-Squash-Crust-Pizza_s4x3.jpg.rend.hgtvcom.616.462.suffix/1538057934894.jpeg"), 3);
+      service.save(
+          new Review("The best 'real' pizza you may ever have", "I love some good Neapolitan pizza, especially if it has some garlic in it.",
+              4,
+              "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2018/9/27/0/KC1812_Spaghetti-Squash-Crust-Pizza_s4x3.jpg.rend.hgtvcom.616.462.suffix/1538057934894.jpeg"), 2);
     }
   }
 }
